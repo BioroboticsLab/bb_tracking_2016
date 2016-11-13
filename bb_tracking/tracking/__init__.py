4dd467f61241cd98e2b7d3bba3120b2b4ea64580
Benjamin Rosemann <benjamin.rosemann@fu-berlin.de>
2016-11-13 17:37:41 +0100

    Separate training data generation and training
    The generation of training data is now separated into it's own function.
    This helps use cases where you don't have a classifier or want to use
    your own custom training algorithms.

--- bb_tracking/tracking/__init__.py
+++ bb_tracking/tracking/__init__.py
@@ -12,7 +12,7 @@ from .scoring import bit_array_to_int_v, score_id_sim, score_id_sim_v,\
     distance_orientations, distance_orientations_v, distance_positions_v
 
 from .tracking import make_detection_score_fun, make_track_score_fun
-from .training import train_and_evaluate, train_bin_clf
+from .training import train_and_evaluate, train_bin_clf, generate_learning_data
 
 from .walker import SimpleWalker
 
@@ -20,5 +20,5 @@ __all__ = ['bit_array_to_int_v', 'score_id_sim', 'score_id_sim_v', 'score_id_sim
            'score_id_sim_orientation_v', 'score_id_sim_rotating', 'score_id_sim_rotating_v',
            'score_id_sim_tracks_median_v',
            'distance_orientations', 'distance_orientations_v', 'distance_positions_v',
-           'train_and_evaluate', 'train_bin_clf',
+           'train_and_evaluate', 'train_bin_clf', 'generate_learning_data',
            'make_detection_score_fun', 'make_track_score_fun', 'SimpleWalker']
--- bb_tracking/tracking/training.py
+++ bb_tracking/tracking/training.py
@@ -4,7 +4,6 @@ To help generating learning data we provide some helpers. The provided functions
 the generation of learning data with training and evaluation of classifiers. You may also use the
 generated learning data and scoring functions for your own training and evaluation process.
 """
-# pylint:disable=too-many-locals
 from __future__ import print_function
 import copy
 import numpy as np
@@ -13,6 +12,81 @@ from sklearn.metrics import classification_report, roc_curve, auc
 from ..data import DataWrapperTracks
 from ..validation import Validator, convert_validated_to_pandas
 from .walker import SimpleWalker
+
+
+def generate_learning_data(dw_truth, features, frame_diff, radius):
+    """Function to generate learning data using truth data.
+
+    The features are are the same that could be used to train a binary classifier.
+
+    Warning:
+        It is recommended to use an :obj:`collections.OrderedDict` instead of a regular :obj:`dict`
+        for `features` because the order in :obj:`dict` is not guaranteed.
+
+    Arguments:
+        dw_truth (:class:`.DataWrapperTruth`): :class:`.DataWrapperTruth` with truth data
+        features (:obj:`dict`): ``{:attr:`feature`: score_fun(tracks, frame_objects_test)}`` mapping
+        frame_diff (int): after n frames a close track if no matching object is found
+        radius (int): radius in image coordinates to restrict neighborhood search
+
+    Returns:
+        tuple: tuple containing:
+
+            - **x_data** (:obj:`np.array`): The learning data
+            - **y_data** (:obj:`np.array`): The correct classes for the learning data
+    """
+    def score_fun_learning(tracks_path, frame_objects_test):
+        """Scoring function that fills ``learning_data`` and ``learning_target`` for classifier.
+
+        Arguments:
+            tracks_path (:obj:`list` of :obj:`.Track`): A list with the :obj:`.Track` objects
+            frame_objects_test (:obj:`list` of :obj:`.Track` or :obj:`.Detection`): A list with
+                either :obj:`.Track` or :obj:`.Detection` that is scored with `tracks_path`.
+
+        Returns:
+            :obj:`list`: distintive weights to allow the walker to continue on the truth path
+        """
+        for key, fun in features.items():
+            learning_data[key].extend(fun(tracks_path, frame_objects_test))
+        weights = []
+        for track_path, frame_object_test in zip(tracks_path, frame_objects_test):
+            truth_path = dw_truth.get_truthid(track_path)
+            truth_test = dw_truth.get_truthid(frame_object_test)
+            if truth_path is None and truth_test is None:  # pragma: no cover
+                # avoid accidentally learning from False Positive Tracks
+                weights.append(np.inf)
+                learning_target.append(-1)
+            elif truth_path == truth_test:
+                weights.append(0)
+                learning_target.append(1)
+            else:
+                weights.append(np.inf)
+                learning_target.append(0)
+        return weights
+
+    learning_data = {key: list() for key in features.keys()}
+    learning_target = list()
+    walker = SimpleWalker(dw_truth, score_fun_learning, frame_diff, radius)
+    tracks = walker.calc_tracks()
+
+    # just make sure that we correctly "tracked" the truth data
+    scores = convert_validated_to_pandas(
+        Validator(dw_truth).validate(
+            tracks, frame_diff - 1, gap_l=False, gap_r=False, cam_gap=True))
+    # remove scores with deletes for track training: we have missing fragments in the test data
+    if isinstance(dw_truth, DataWrapperTracks):
+        scores = scores[scores.deletes == 0]
+    assert np.all(scores.inserts == 0) and np.all(scores.deletes == 0) and np.all(scores.value >= 1)
+
+    x_data = np.array([learning_data[key] for key in features.keys()]).T
+    y_data = np.array(learning_target)
+    y_data[y_data == 0] = False
+    y_data[y_data == 1] = True
+    mask = np.ones(len(y_data), dtype=bool)
+    mask[y_data == -1] = False
+    y_data = y_data[mask]
+    x_data = x_data[mask, ]
+    return x_data, y_data
 
 
 def train_and_evaluate(clf, x_data, y_data, verbose=False, **kwargs):
@@ -30,24 +104,23 @@ def train_and_evaluate(clf, x_data, y_data, verbose=False, **kwargs):
         verbose (Optional bool): if true calculates accuracy, 10-fold cross validation...
         **kwargs (:obj:`dict`): Keyword arguments for ``clf.fit()``.
     """
-    sss = StratifiedShuffleSplit(n_splits=1)
-    train_indices, test_indices = list(sss.split(x_data, y_data))[0]
-    x_train, x_test, = x_data[train_indices], x_data[test_indices]
-    y_train, y_test = y_data[train_indices], y_data[test_indices]
-    clf.fit(x_train, y_train, **kwargs)
+    train_indices, test_indices = list(StratifiedShuffleSplit(n_splits=1).split(x_data, y_data))[0]
+    clf.fit(x_data[train_indices], y_data[train_indices], **kwargs)
     if verbose:
         if hasattr(clf, "predict_proba"):
-            y_score = clf.predict_proba(x_test)[:, 1]
+            y_score = clf.predict_proba(x_data[test_indices])[:, 1]
         else:
-            y_score = clf.decision_function(x_test)
-        fpr, tpr, _ = roc_curve(y_test, y_score)
+            y_score = clf.decision_function(x_data[test_indices])
+        fpr, tpr, _ = roc_curve(y_data[test_indices], y_score)
         print("ROC AUC: {:.4f}".format(auc(fpr, tpr)))
-        print("Accuracy on training set: {:.4f}".format(clf.score(x_train, y_train)))
-        print("Accuracy on testing set: {:.4f}.".format(clf.score(x_test, y_test)))
+        print("Accuracy on training set: {:.4f}".format(clf.score(x_data[train_indices],
+                                                                  y_data[train_indices])))
+        print("Accuracy on testing set: {:.4f}.".format(clf.score(x_data[test_indices],
+                                                                  y_data[test_indices])))
         y_pred = cross_val_predict(clf, x_data, y_data, cv=10, fit_params=kwargs)
         print("Classification Report (10 fold cross validation):")
         print(classification_report(y_data, y_pred, digits=4))
-        clf.fit(x_train, y_train, **kwargs)
+        clf.fit(x_data[train_indices], y_data[train_indices], **kwargs)
 
 
 def train_bin_clf(clf, dw_truth, features, frame_diff, radius, **kwargs):
@@ -82,25 +155,6 @@ def train_bin_clf(clf, dw_truth, features, frame_diff, radius, **kwargs):
             - **y_data** (:obj:`np.array`): The correct classes for the learning data
             - **score_fun** (:obj:`func`): A generic scoring function for the Classifier
     """
-    def score_fun_learning(tracks_path, frame_objects_test):
-        """Scoring function that fills ``learning_data`` and ``learning_target`` for classifier."""
-        for key, fun in features.items():
-            learning_data[key].extend(fun(tracks_path, frame_objects_test))
-        weights = []
-        for track_path, frame_object_test in zip(tracks_path, frame_objects_test):
-            truth_path = dw_truth.get_truthid(track_path)
-            truth_test = dw_truth.get_truthid(frame_object_test)
-            if truth_path is None and truth_test is None:  # pragma: no cover
-                # avoid accidentally learning from False Positive Tracks
-                weights.append(np.inf)
-                learning_target.append(-1)
-            elif truth_path == truth_test:
-                weights.append(0)
-                learning_target.append(1)
-            else:
-                weights.append(np.inf)
-                learning_target.append(0)
-        return weights
 
     def score_fun_generic(tracks_path, frame_objects_test):
         """Generic scoring function that might be used together with the trained classifier.
@@ -131,27 +185,6 @@ def train_bin_clf(clf, dw_truth, features, frame_diff, radius, **kwargs):
 
     # make a copy of the feature functions because the training and scoring functions depend on them
     features = copy.deepcopy(features)
-    learning_data = {key: list() for key in features.keys()}
-    learning_target = list()
-    walker = SimpleWalker(dw_truth, score_fun_learning, frame_diff, radius)
-    tracks = walker.calc_tracks()
-
-    # just make sure that we correctly "tracked" the truth data
-    scores = convert_validated_to_pandas(
-        Validator(dw_truth).validate(
-            tracks, frame_diff - 1, gap_l=False, gap_r=False, cam_gap=True))
-    # remove scores with deletes for track training: we have missing fragments in the test data
-    if isinstance(dw_truth, DataWrapperTracks):
-        scores = scores[scores.deletes == 0]
-    assert np.all(scores.inserts == 0) and np.all(scores.deletes == 0) and np.all(scores.value >= 1)
-
-    x_data = np.array([learning_data[key] for key in features.keys()]).T
-    y_data = np.array(learning_target)
-    y_data[y_data == 0] = False
-    y_data[y_data == 1] = True
-    mask = np.ones(len(y_data), dtype=bool)
-    mask[y_data == -1] = False
-    y_data = y_data[mask]
-    x_data = x_data[mask, ]
+    x_data, y_data = generate_learning_data(dw_truth, features, frame_diff, radius)
     train_and_evaluate(clf, x_data, y_data, **kwargs)
     return x_data, y_data, score_fun_generic
